8e4562fca7b931d54d95eaf0217bddf5b6baf849
l-ashish-gupta-l <ashish3072002@gmail.com>
2024-02-26 10:42:33 +0530

    added typeing effect

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import HomeBg from "../assets/HomeBg.jpg"
 import Mainherobg from "../assets/Mainherobg.png"
+import { TypeAnimation } from 'react-type-animation';
 
 
 function Mainbtn() {
@@ -10,8 +11,6 @@ function Mainbtn() {
         </button>
     )
 }
-
-
 
 function LandingPage() {
     return (
@@ -25,18 +24,29 @@ function LandingPage() {
                     <div className=' absolute  font-["Bebas"] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  flex flex-col  items-center'>
                         <h2 className='uppercase text-2xl'>Experience the taste of INDIA</h2>
                         <h1 className='text-stroke stroke-white mt-3 text-[7vw] whitespace-nowrap leading-none'>GREAT DINING</h1>
-                        <h1 className='text-white text-[6vw] whitespace-nowrap leading-none'>experience</h1>
+                        <TypeAnimation
+                            sequence={[
+                                'Experence',
+                                2000,
+                                'RESTAURANT',
+                                2000,
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            className="text-8xl text-white"
+                            repeat={Infinity}
+                            cursor={false}
+                        />
                         <Mainbtn />
                     </div>
                 </div>
                 <div className='circle absolute right-0 top-[70%] w-[150px] h-[150px] bg-[#DADADA] rounded-full '>
-                    <div className='text absolute w-full h-full bg-black'>
+                    <div className='text absolute w-full h-full '>
                         <p className='text-white w-full h-full text-sm '>
                             {('THE SWAAD EXPERENCE THE REAL TASTE OF INDIA').split('').map((letter, index) => {
                                 return (
                                     <span className={`rotate-[${index * 8.3}deg]  origin-[0_100%] `}>{letter}</span>
                                 )
-
                             })}
                         </p>
                     </div>
